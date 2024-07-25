c4950fe188709448febb52db74fc1ad3f701ceea
Nicolastroadec <troadecnicolas@hotmail.fr>
2024-07-25 17:08:09 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,38 @@
+
+import './App.css'
+import { ChangeEvent, useState } from 'react'
+
+function TodoList() {
+  const [listItems, setListItems] = useState<string[]>([]);
+  const [inputValue, setInputValue] = useState<string>('');
+  const handleInputValue = (event: ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value);
+  }
+
+  const handleClick = () => {
+    setListItems([...listItems, inputValue]);
+    setInputValue('');
+  }
+
+  const deleteItem = (itemToDelete: string) => {
+    setListItems(listItems.filter(item => item !== itemToDelete));
+  }
+
+  return <div>
+    <h2>Ajouter un élément</h2>
+    <input style={{ marginRight: '20px' }} type="text" value={inputValue} onChange={handleInputValue} />
+    <button onClick={handleClick}>Ajouter l'élément à la liste</button>
+    <ul >
+      {listItems.map((item, index) => (
+        <div style={{ display: 'flex', alignItems: 'center', marginBottom: '20px' }} key={index}>
+          <li style={{ marginRight: '20px' }}>{item}</li>
+          <button onClick={() => deleteItem(item)}>X</button></div>
+      ))}
+    </ul>
+
+  </div>;
+}
+
+export default TodoList;
+
+
